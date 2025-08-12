5d3021c27e4faa206faf16aabace0b001d29395e
Jesse Shaw <JesseShawCodes@gmail.com>
2025-08-11 22:57:53 -0400

    feat: Add TypeScript configuration and new layout component with alert functionality

--- audiograd/src/app/components/Alert.tsx
+++ audiograd/src/app/components/Alert.tsx
@@ -1,4 +1,11 @@
-export default function Alert({message, alertType}) {
+import React from "react";
+
+interface AlertProps {
+  message?: string; // Optional message
+  alertType?: string; // Optional alert type
+}
+
+export default function Alert({ message, alertType }: AlertProps) {
   return (
     <div className={`alert alert-${alertType || 'danger'} border-0`} role="alert">
       <strong>Error:</strong> {message || "The projects backend could not be reached at this time. We are actively working to figure this bug out. Please check back again later!"}
